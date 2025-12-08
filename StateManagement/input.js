dbc49a4b402eeff6f19272953d7c2d6a9c45f58d
jeremy zeta <jeremy_zeta@yahoo.com>
2025-12-08 17:59:55 +0900

    Add StateManagement (TODO: add jumpingstates)

---
+++ StateManagement/input.js
@@ -0,0 +1,37 @@
+export default class InputHandler{
+    constructor(){
+        this.lastkey = '';
+        window.addEventListener('keydown', (e)=>{
+            switch(e.key){
+                case "ArrowLeft":
+                    this.lastkey = "PRESS left";
+                    break
+                case "ArrowRight":
+                    this.lastkey = "PRESS right";
+                    break
+                case "ArrowDown":
+                    this.lastkey = "PRESS down";
+                    break
+                case "ArrowUp":
+                    this.lastkey = "PRESS up";
+                    break
+            }
+        });
+        window.addEventListener('keyup', (e)=>{
+            switch(e.key){
+                case "ArrowLeft":
+                    this.lastkey = "RELEASE left";
+                    break
+                case "ArrowRight":
+                    this.lastkey = "RELEASE right";
+                    break
+                case "ArrowDown":
+                    this.lastkey = "RELEASE down";
+                    break
+                case "ArrowDown":
+                    this.lastkey = "RELEASE up";
+                    break
+            }
+        });
+    }
+}
