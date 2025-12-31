ea05e6942d1c87624a4c03cd2b3d01785adad76a
jeremy zeta <jeremy_zeta@yahoo.com>
2025-12-31 15:32:34 +0900

    Statemanagement: 2canvas overlay 1-background, 2-mainobjects

--- StateManagement/script.js
+++ StateManagement/script.js
@@ -6,9 +6,14 @@ window.addEventListener('load', function(){
     const loading = document.getElementById('loading');
     loading.style.display = 'none';
     const canvas = document.getElementById('canvas1');
+    const canvasbackground = document.getElementById('canvas2');
     const ctx = canvas.getContext('2d');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+    canvasbackground.width = window.innerWidth;
+    canvasbackground.height = window.innerHeight;
+
+    backgrounddraw(canvasbackground);
 
     const player = new Player(canvas.width, canvas.height);
     player.draw(ctx);
@@ -16,6 +21,7 @@ window.addEventListener('load', function(){
 
     function animate(){
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        // ctx.clearRect(player.x, player.y, player.width, player.height); 
         console.log(input.lastkey);
         player.update(input.lastkey);
         player.draw(ctx);
@@ -24,3 +30,19 @@ window.addEventListener('load', function(){
     }
     animate();
 })
+
+
+function backgrounddraw(canvasbackground){
+    // ctxbgd backgroundcolor
+    const ctxbgd = canvasbackground.getContext('2d');
+    for (let i = 0; i < canvasbackground.width; i++) {
+        for (let j = 0; j < canvasbackground.height; j++) {
+            ctxbgd.fillStyle = `rgb(
+        ${Math.floor(255 - 2 * i)}
+        ${Math.floor(255 - 2 * j)}
+        0)`;
+            ctxbgd.fillRect(j * 25, i * 25, 25, 25);
+        }
+    }
+    // ctx backgroundcolor
+}
