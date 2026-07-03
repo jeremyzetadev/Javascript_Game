2ee33b66884524034e1d689d607c9fbcfa77bae3
jeremyzetadev <jeremy_zeta@yahoo.com>
2026-07-03 17:13:23 +0900

    enemy class extract from function to outerscope

--- EnemyMovementPattern2/script.js
+++ EnemyMovementPattern2/script.js
@@ -53,118 +53,6 @@ document.addEventListener('DOMContentLoaded', function(){
         }
     }
 
-    class Enemy{
-        constructor(game){
-            this.game = game;
-            this.x = this.game.width;
-            this.y = Math.random() * this.game.height;
-            this.width = 100;
-            this.height = 100;
-            this.frameX;
-            this.maxFrame = 5;
-            this.frameInterval = 100;
-            this.frameTimer = 0;
-
-            this.markForDeletion = false;
-        }
-
-        update(deltaTime){
-            this.x -= this.vx * deltaTime;
-            // remove enemies
-            if(this.x < 0-this.width) this.markForDeletion=true;
-            // animationframes
-            if(this.frameTimer>this.frameInterval){
-                if(this.frameX < this.maxFrame) this.frameX++ 
-                else this.frameX = 0;
-                this.frameTimer = 0;
-            }else{
-                this.frameTimer += deltaTime;
-            }
-        }
-
-        draw(ctx){
-            // ctx.fillRect(this.x, this.y, this.width, this.height);
-            // ctx.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
-            // animationframes
-            ctx.drawImage(this.image, this.frameX * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
-        }
-    }
-
-    class Worm extends Enemy{
-        constructor(game){
-            super(game)
-            this.spriteWidth = 229;
-            this.spriteHeight = 171;
-            this.width = this.spriteWidth/2;
-            this.height = this.spriteHeight/2;
-            this.x = this.game.width;
-            this.y = this.game.height-this.height //Math.random() * this.game.height;
-            this.image = worm; //direct access id of html in dom (no need to use document.getElementById or document.getClassSelector)
-            this.vx = Math.random() * 0.1 + 0.1;
-        }
-    }
-
-    class Ghost extends Enemy{
-        constructor(game){
-            super(game)
-            this.spriteWidth = 261;
-            this.spriteHeight = 209;
-            this.width = this.spriteWidth/2;
-            this.height = this.spriteHeight/2;
-            this.x = this.game.width;
-            this.y = Math.random() * this.game.height * 0.6;
-            this.image = ghost; //direct access id of html in dom (no need to use document.getElementById or document.getClassSelector)
-            this.vx = Math.random() * 0.2 + 0.1;
-            this.angle = 0;
-            this.curve = Math.random() * 3;
-        }
-
-        update(deltaTime){
-            super.update(deltaTime);
-            this.y +=  Math.sin(this.angle) * this.curve;
-            this.angle += 0.05;
-        }
-
-        draw(ctx){
-            ctx.save();
-            ctx.globalAlpha = 0.4;
-            ctx.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
-            super.draw(ctx);
-            ctx.restore();
-        }
-    }
-
-    class Spider extends Enemy{
-        constructor(game){
-            super(game)
-            this.spriteWidth = 310;
-            this.spriteHeight = 175;
-            this.width = this.spriteWidth/2;
-            this.height = this.spriteHeight/2;
-            this.x = Math.random() * this.game.width;
-            this.y = 0-this.height //Math.random() * this.game.height;
-            this.image = spider; //direct access id of html in dom (no need to use document.getElementById or document.getClassSelector)
-            this.vx = 0;
-            this.vy = Math.random() * 0.1 + 2;
-            this.maxLengthY = Math.random() * game.height;
-        }
-
-        update(deltaTime){
-            super.update(deltaTime);
-            if (this.y < 0-this.height*2) this.markForDeletion = true;
-            this.y +=  this.vy;
-            if (this.y > this.maxLengthY) this.vy *=-1;
-        }
-
-        draw(ctx){
-            ctx.beginPath();
-            ctx.moveTo(this.x + this.width/2, 0);
-            ctx.lineTo(this.x + this.width/2, this.y + 10);
-            ctx.stroke();
-            super.draw(ctx);
-        }
-    }
-
     const game = new Game(ctx, canvas.width, canvas.height);
     let lastTime = 1;
     function animate(timeStamp){
@@ -179,4 +67,114 @@ document.addEventListener('DOMContentLoaded', function(){
     animate(0);
 });
 
+class Enemy{
+    constructor(game){
+        this.game = game;
+        this.x = this.game.width;
+        this.y = Math.random() * this.game.height;
+        this.width = 100;
+        this.height = 100;
+        this.frameX;
+        this.maxFrame = 5;
+        this.frameInterval = 100;
+        this.frameTimer = 0;
 
+        this.markForDeletion = false;
+    }
+
+    update(deltaTime){
+        this.x -= this.vx * deltaTime;
+        // remove enemies
+        if(this.x < 0-this.width) this.markForDeletion=true;
+        // animationframes
+        if(this.frameTimer>this.frameInterval){
+            if(this.frameX < this.maxFrame) this.frameX++ 
+            else this.frameX = 0;
+            this.frameTimer = 0;
+        }else{
+            this.frameTimer += deltaTime;
+        }
+    }
+
+    draw(ctx){
+        // ctx.fillRect(this.x, this.y, this.width, this.height);
+        // ctx.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        // animationframes
+        ctx.drawImage(this.image, this.frameX * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+    }
+}
+
+class Worm extends Enemy{
+    constructor(game){
+        super(game)
+        this.spriteWidth = 229;
+        this.spriteHeight = 171;
+        this.width = this.spriteWidth/2;
+        this.height = this.spriteHeight/2;
+        this.x = this.game.width;
+        this.y = this.game.height-this.height //Math.random() * this.game.height;
+        this.image = worm; //direct access id of html in dom (no need to use document.getElementById or document.getClassSelector)
+        this.vx = Math.random() * 0.1 + 0.1;
+    }
+}
+
+class Ghost extends Enemy{
+    constructor(game){
+        super(game)
+        this.spriteWidth = 261;
+        this.spriteHeight = 209;
+        this.width = this.spriteWidth/2;
+        this.height = this.spriteHeight/2;
+        this.x = this.game.width;
+        this.y = Math.random() * this.game.height * 0.6;
+        this.image = ghost; //direct access id of html in dom (no need to use document.getElementById or document.getClassSelector)
+        this.vx = Math.random() * 0.2 + 0.1;
+        this.angle = 0;
+        this.curve = Math.random() * 3;
+    }
+
+    update(deltaTime){
+        super.update(deltaTime);
+        this.y +=  Math.sin(this.angle) * this.curve;
+        this.angle += 0.05;
+    }
+
+    draw(ctx){
+        ctx.save();
+        ctx.globalAlpha = 0.4;
+        ctx.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        super.draw(ctx);
+        ctx.restore();
+    }
+}
+
+class Spider extends Enemy{
+    constructor(game){
+        super(game)
+        this.spriteWidth = 310;
+        this.spriteHeight = 175;
+        this.width = this.spriteWidth/2;
+        this.height = this.spriteHeight/2;
+        this.x = Math.random() * this.game.width;
+        this.y = 0-this.height //Math.random() * this.game.height;
+        this.image = spider; //direct access id of html in dom (no need to use document.getElementById or document.getClassSelector)
+        this.vx = 0;
+        this.vy = Math.random() * 0.1 + 2;
+        this.maxLengthY = Math.random() * game.height;
+    }
+
+    update(deltaTime){
+        super.update(deltaTime);
+        if (this.y < 0-this.height*2) this.markForDeletion = true;
+        this.y +=  this.vy;
+        if (this.y > this.maxLengthY) this.vy *=-1;
+    }
+
+    draw(ctx){
+        ctx.beginPath();
+        ctx.moveTo(this.x + this.width/2, 0);
+        ctx.lineTo(this.x + this.width/2, this.y + 10);
+        ctx.stroke();
+        super.draw(ctx);
+    }
+}
